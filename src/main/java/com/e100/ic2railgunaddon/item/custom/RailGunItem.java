0d1fea606de782fb8097058f306a3dcf2eb0559b
minec <minec@LAPTOP_LENOVO>
2024-06-05 20:21:32 +1000

    Implemented sound when the user fires the weapon, also added fire placement and set mobs on fire as well. Added error checking to see if weapon is out of charge.

--- src/main/java/com/e100/ic2railgunaddon/item/custom/RailGunItem.java
+++ src/main/java/com/e100/ic2railgunaddon/item/custom/RailGunItem.java
@@ -5,9 +5,12 @@ import org.antlr.v4.runtime.misc.NotNull;
 import com.e100.ic2railgunaddon.item.AmmoManager;
 import com.e100.ic2railgunaddon.item.ModCreativeModeTab;
 
+import ic2.core.platform.registries.IC2Sounds;
 import ic2.api.items.electric.ElectricItem;
 import ic2.api.items.electric.IDamagelessElectricItem;
 import ic2.api.items.electric.IElectricEnchantable;
+import ic2.core.IC2;
+import ic2.core.audio.AudioManager;
 import ic2.core.item.base.IC2ElectricItem;
 import ic2.core.platform.rendering.IC2Textures;
 import ic2.core.platform.rendering.features.item.ICustomItemModelTransform;
@@ -16,6 +19,8 @@ import ic2.core.utils.helpers.StackUtil;
 import ic2.core.utils.plugins.IRegistryProvider;
 import ic2.core.utils.tooltips.IAdvancedTooltip;
 import ic2.core.utils.tooltips.ToolTipHelper;
+import ic2.core.platform.registries.IC2Sounds;
+
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.InteractionResultHolder;
@@ -28,22 +33,34 @@ import net.minecraft.world.item.enchantment.Enchantment;
 import net.minecraft.world.item.enchantment.EnchantmentCategory;
 import net.minecraft.world.item.enchantment.Enchantments;
 import net.minecraft.world.item.UseAnim;
+import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.level.ClipContext;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.BaseFireBlock;
+import net.minecraft.world.level.block.CampfireBlock;
+import net.minecraft.world.level.block.CandleBlock;
+import net.minecraft.world.level.block.CandleCakeBlock;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.gameevent.GameEvent;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.advancements.CriteriaTriggers;
+import net.minecraft.core.BlockPos;
 import net.minecraft.core.NonNullList;
+import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.stats.Stats;
 import net.minecraft.world.phys.HitResult;
 import net.minecraft.world.phys.Vec3;
+import net.minecraftforge.common.extensions.IForgeItem;
 import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.EntityHitResult;
 
-public class RailGunItem extends IC2ElectricItem implements IDamagelessElectricItem, IAdvancedTooltip, IElectricEnchantable {
+public class RailGunItem extends IC2ElectricItem implements IDamagelessElectricItem, IAdvancedTooltip, IElectricEnchantable, IForgeItem {
 	
 	public RailGunItem() {
         super("combat_laser");
@@ -52,10 +69,10 @@ public class RailGunItem extends IC2ElectricItem implements IDamagelessElectricI
         this.transferLimit = 512;
     }
 	
-	
+	/*@Override
 	public UseAnim getUseAnimation(ItemStack stack) {
 		return UseAnim.BOW;
-	}
+	}*/
 
 	@Override
 	public InteractionResult getEnchantmentCompatibility(ItemStack arg0, Enchantment arg1) {
@@ -110,20 +127,55 @@ public class RailGunItem extends IC2ElectricItem implements IDamagelessElectricI
     public InteractionResultHolder<ItemStack> use(Level pLevel, Player pPlayer, InteractionHand pHand){
 		ItemStack itemstack = pPlayer.getItemInHand(pHand);
 		
-		EntityHitResult result = rayTraceEntities(pLevel, (LivingEntity)pPlayer, false, 60.0D);
 		
-		if (result != null) {
-			EntityHitResult entityHitResult = (EntityHitResult) result;
-            entityHitResult.getEntity().hurt(DamageSource.playerAttack(pPlayer), getDamage());
-            ElectricItem.MANAGER.discharge(itemstack, 1000, getTier(itemstack), true, false, false);
-            pPlayer.getCooldowns().addCooldown(this, 20);
-            //pPlayer.getAttackAnim(1.0f);
+		if (ElectricItem.MANAGER.getCharge(itemstack) > 1000) {
+		   EntityHitResult result = rayTraceEntities(pLevel, (LivingEntity)pPlayer, false, 60.0D);//detects if entity was hit
+		   
+		   HitResult result1 = pPlayer.pick(60.0D, 0.0F, false);//detects if block was hit
+		
+		   if (result != null) {
+			   EntityHitResult entityHitResult = (EntityHitResult) result;
+               entityHitResult.getEntity().hurt(DamageSource.playerAttack(pPlayer), getDamage());
+               entityHitResult.getEntity().setSecondsOnFire(5);
+               ElectricItem.MANAGER.discharge(itemstack, 1000, getTier(itemstack), true, false, false);
+               pPlayer.getCooldowns().addCooldown(this, 20); 
+               IC2.AUDIO.playSound(pPlayer, IC2Sounds.LASER_DEFAULT, AudioManager.SoundType.ITEM);
+               
+		   }else if(result1.getType() == HitResult.Type.BLOCK){//checks if a block was hit
+			   BlockHitResult blockHitResult = (BlockHitResult) result1;
+               BlockPos blockPos = blockHitResult.getBlockPos().relative(blockHitResult.getDirection());
+               if(pLevel.getBlockState(blockPos).isAir()) {
+            	   pLevel.setBlockAndUpdate(blockPos, BaseFireBlock.getState(pLevel, blockPos));
+                   ElectricItem.MANAGER.discharge(itemstack, 1000, getTier(itemstack), true, false, false);
+                   pPlayer.getCooldowns().addCooldown(this, 20);
+                   IC2.AUDIO.playSound(pPlayer, IC2Sounds.LASER_DEFAULT, AudioManager.SoundType.ITEM);
+                   pPlayer.sendSystemMessage(Component.literal("Fire Placed"));//remember these are for testing
+                   
+               }else {
+                   pPlayer.sendSystemMessage(Component.literal("Target block is not air"));//remember these are for testing
+                   ElectricItem.MANAGER.discharge(itemstack, 1000, getTier(itemstack), true, false, false);
+                   IC2.AUDIO.playSound(pPlayer, IC2Sounds.LASER_DEFAULT, AudioManager.SoundType.ITEM);
+                   pPlayer.getCooldowns().addCooldown(this, 20);
+               }
+			   			   			   
+		   }else {
+			   pPlayer.sendSystemMessage(Component.literal("No target locked"));//remember these are for testing
+			   ElectricItem.MANAGER.discharge(itemstack, 1000, getTier(itemstack), true, false, false);
+               IC2.AUDIO.playSound(pPlayer, IC2Sounds.LASER_DEFAULT, AudioManager.SoundType.ITEM);
+			   pPlayer.getCooldowns().addCooldown(this, 20); 
+		   }
+		   
 		}else {
-			pPlayer.sendSystemMessage(Component.literal("No target locked"));
-			pPlayer.getCooldowns().addCooldown(this, 20);
-		}
+            pPlayer.sendSystemMessage(Component.literal("Not enough charge to fire"));//remember these are for testing
+            pPlayer.getCooldowns().addCooldown(this, 20);
+        }
 		
-		return super.use(pLevel, pPlayer, pHand);
+	return super.use(pLevel, pPlayer, pHand);    
+    }
+    
+    @Override
+    public boolean shouldCauseReequipAnimation(ItemStack oldStack, ItemStack newStack, boolean slotChanged) {
+        return slotChanged || !ItemStack.isSame(oldStack, newStack);
     }
     
     private float getDamage() {
